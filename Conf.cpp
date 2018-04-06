76511defae1c5ecfecee3f6845acfda499dac136
Andy CA6JAU <juribeparada@gmail.com>
2018-04-06 14:25:52 -0300

    Remove quotes from config file values

--- Conf.cpp
+++ Conf.cpp
@@ -121,6 +121,16 @@ bool CConf::read()
 			continue;
 
 		char* value = ::strtok(NULL, "\r\n");
+		if (value == NULL)
+			continue;
+
+		// Remove quotes from the value
+		size_t len = ::strlen(value);
+		if (len > 1U && *value == '"' && value[len - 1U] == '"') {
+			value[len - 1U] = '\0';
+			value++;
+		}
+
 		if (section == SECTION_NXDN_NETWORK) {
 			if (::strcmp(key, "Callsign") == 0) {
 				// Convert the callsign to upper case
